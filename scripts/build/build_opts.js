a3769517bfb5e7d0e5da8b18f3e46dbaad59c3ae
Yama-Tomo <mazda13bturbo@gmail.com>
2021-09-24 16:24:57 +0900

    refactor: esbuild に渡すオプションを別ファイルへ切り出し

---
+++ scripts/build/build_opts.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const alias = require('esbuild-plugin-alias');
+
+const shims = {
+  empty: path.resolve(__dirname, '..', '..', 'src', 'shims', 'empty.ts'),
+  buffer: path.resolve(__dirname, '..', '..', 'src', 'shims', 'buffer.ts'),
+};
+
+module.exports = () => ({
+  entryPoints: [path.resolve(__dirname, '..', '..', 'src', 'index.ts')],
+  outfile: path.resolve(__dirname, '..', '..', 'dist', 'index.js'),
+  bundle: true,
+  minify: true,
+  define: {
+    global: 'self',
+    'process.env.NODE_ENV': process.env.NODE_ENV ? `'${process.env.NODE_ENV}'` : `'development'`,
+  },
+  plugins: [
+    alias({
+      crypto: shims.empty,
+      zlib: shims.empty,
+      https: shims.empty,
+      os: shims.empty,
+      buffer: shims.buffer,
+    }),
+  ],
+});
--- scripts/build/cloudflare.js
+++ scripts/build/cloudflare.js
@@ -1,33 +1,7 @@
-const path = require('path');
 const build = require('esbuild');
-const alias = require('esbuild-plugin-alias');
+const opts = require('./build_opts');
 
-const shims = {
-  empty: path.resolve(__dirname, '..', '..', 'src', 'shims', 'empty.ts'),
-  buffer: path.resolve(__dirname, '..', '..', 'src', 'shims', 'buffer.ts'),
-};
-
-build
-  .build({
-    entryPoints: [path.resolve(__dirname, '..', '..', 'src', 'index.ts')],
-    outfile: path.resolve(__dirname, '..', '..', 'dist', 'index.js'),
-    bundle: true,
-    minify: true,
-    define: {
-      global: 'self',
-      'process.env.NODE_ENV': process.env.NODE_ENV ? `'${process.env.NODE_ENV}'` : `'development'`,
-    },
-    plugins: [
-      alias({
-        crypto: shims.empty,
-        zlib: shims.empty,
-        https: shims.empty,
-        os: shims.empty,
-        buffer: shims.buffer,
-      }),
-    ],
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+build.build(opts()).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
