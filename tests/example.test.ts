59b89acae5aad2b1ca07ae74dae538223c87a7cd
Yama-Tomo <yam4.tomo@gmail.com>
2021-10-05 13:28:21 +0900

    test: graphql のレスポンスが正しいかのテストの追加

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,5 +1,12 @@
+import { doRequest } from './request_helper';
+
 describe('example', () => {
   it('should be correct calculation', () => {
     expect(1 + 2).toBe(3);
   });
+
+  it('should be correct response', async () => {
+    const response = await doRequest(`query ExampleQuery { hello }`);
+    expect(response).toEqual({ data: { hello: 'Hello world!' } });
+  }, 10000);
 });
---
+++ tests/request_helper.ts
@@ -0,0 +1,22 @@
+import { CFOpts, createServer } from '../src/server';
+
+const doRequest = async (
+  query: string,
+  opts?: Partial<{ cfOpts: CFOpts; headers: HeadersInit }>
+): Promise<any> => {
+  const server = createServer(opts?.cfOpts);
+
+  const request = new Request(`/`, {
+    body: JSON.stringify({ query }),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...(opts?.headers ? opts.headers : {}),
+    },
+  });
+
+  const response = await server.dispatch(request);
+  return response.json();
+};
+
+export { doRequest };
