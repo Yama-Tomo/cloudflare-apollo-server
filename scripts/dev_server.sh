6d151d4fa543df69cc7bf46799ef79d04a1b8508
Yama-Tomo <yam4.tomo@gmail.com>
2021-10-08 21:10:24 +0900

    refactor: 開発サーバの起動をシェルへ切り出し

---
+++ scripts/dev_server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+dirname=$(cd $(dirname $0);pwd)
+
+pnpm -- node -r esbuild-register ${dirname}/programs/prepare_cfworker_dev_ext.ts
+CFWORKER_CUSTOM_BUNDLER=${dirname}/programs/.cfworker_dev_ext.js pnpm -- cfworker run -w ./src/index.ts
+
